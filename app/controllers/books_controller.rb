1ca13d764e7357525b075ef3b282d3404d664719
GotouAsahi <gotou.asahi.infratop@gmail.com>
2024-02-20 19:10:19 +0900

    slick_finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  before_action :ensure_correct_user, only: %i[edit update destroy]
 
   def show
     @book = Book.find(params[:id])
@@ -10,27 +10,27 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+    @boards = Book.order(created_at: :desc).limit(4)
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book), notice: 'You have created book successfully.'
     else
       @books = Book.all
       render 'index'
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book), notice: 'You have updated book successfully.'
     else
-      render "edit"
+      render 'edit'
     end
   end
 
@@ -47,8 +47,8 @@ class BooksController < ApplicationController
 
   def ensure_correct_user
     @book = Book.find(params[:id])
-    unless @book.user == current_user
-      redirect_to books_path
-    end
+    return if @book.user == current_user
+
+    redirect_to books_path
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,19 +4,17 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import jQuery from "jquery"
+// import jQuery from "jquery"
 import "popper.js"
 import "bootstrap"
 
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
+require("./slick")
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
-
-global.$ = jQuery;
-window.$ = jQuery;
